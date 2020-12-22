e3a99c8efcf4354f2d1187bca3f4168cf05d73ac
Afzal Ali <afzalali1999@gmail.com>
2020-12-22 09:52:38 -0500

    netifly deploy

---
+++ build/precache-manifest.7627a0a773271924279f6b52818e97a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53120d6d129fc44718a1",
+    "url": "/static/css/main.6c416f2e.chunk.css"
+  },
+  {
+    "revision": "53120d6d129fc44718a1",
+    "url": "/static/js/main.53120d6d.chunk.js"
+  },
+  {
+    "revision": "02ba3405e7481ccc0a43",
+    "url": "/static/js/1.02ba3405.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "53be67f049a5c3891dc61ea0bfc042e8",
+    "url": "/index.html"
+  }
+];
